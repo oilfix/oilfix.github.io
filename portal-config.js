b838017ea5020dcddb8a6d39aede48c956006754
oilfix <herim2468007@gmail.com>
2026-08-29 05:50:04 +0900

    Add OILFIX staff portal

---
+++ portal-config.js
@@ -0,0 +1,2 @@
+// Apps Script에서 "웹 앱"으로 배포한 뒤, 끝이 /exec 인 주소를 아래에 넣으세요.
+window.OILFIX_BACKEND_URL = "https://script.google.com/macros/s/AKfycbxB-lugbdvCjyKpy6XYTHF1NrORJZf8Z6-P10Wzu9Pqv6tSiB4AhD1186pVTJ-Gg8sV/exec";
