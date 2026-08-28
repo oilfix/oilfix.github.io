893ae49b336d9084878a9feebc148306bdf3e9cb
oilfix <herim2468007@gmail.com>
2026-08-29 07:58:03 +0900

    Delete portal-config.js

--- portal-config.js
+++
@@ -1,2 +0,0 @@
-// OILFIX 직원 장부 Google Apps Script 백엔드
-window.OILFIX_BACKEND_URL = "https://script.google.com/macros/s/AKfycbyY2A7yD8WtSVxXrK2F9KPaAmfmbRWAm7vADCTlBlR1MSjkzk13TrsipwjoJbYGNCOO/exec";
