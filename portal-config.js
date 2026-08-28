cf551fbbbc7a7e8c6fdb52f37ec8315a7105fa8d
oilfix <herim2468007@gmail.com>
2026-08-29 06:23:52 +0900

    Update portal-config.js

--- portal-config.js
+++ portal-config.js
@@ -1,2 +1,2 @@
 // OILFIX 직원 장부 Google Apps Script 백엔드
-window.OILFIX_BACKEND_URL = "https://script.google.com/macros/s/AKfycbxB-lugbdvCjyKpy6XYTHF1NrORJZf8Z6-P10Wzu9Pqv6tSiB4AhD1186pVTJ-Gg8sV/exec";
+window.OILFIX_BACKEND_URL = "https://script.google.com/macros/s/AKfycbyY2A7yD8WtSVxXrK2F9KPaAmfmbRWAm7vADCTlBlR1MSjkzk13TrsipwjoJbYGNCOO/exec";
